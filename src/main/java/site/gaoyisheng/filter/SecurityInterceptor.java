566a0cdafdba1c81155af3acffc162eaac6c6539
Gao Yisheng <1160139211@qq.com>
2017-11-24 22:25:09 +0800

    admin操作安全检查过滤器OK

---
+++ src/main/java/site/gaoyisheng/filter/SecurityInterceptor.java
@@ -0,0 +1,69 @@
+/*
+ * file_name: SecurityInterceptor.java
+ *
+ * Copyright GaoYisheng Corporation 2017
+ *
+ * License：
+ * date： 2017年11月24日 下午10:00:31
+ *       https://www.gaoyisheng.site
+ *       https://github.com/timo1160139211
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package site.gaoyisheng.filter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import site.gaoyisheng.pojo.User;
+
+/**
+ * 检查 admin 操作的用户是否为 "admin"
+ * @author gaoyisheng
+ *
+ */
+public class SecurityInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		String requestURI = request.getRequestURI();
+
+		//如果包含 admin的操作.
+		if (requestURI.startsWith("/trans/admin")) {
+			// 说明处在编辑的页面
+			User currentUser = (User) request.getSession().getAttribute("currentUser");
+			
+			//已检查登陆. 此处仅检查安全
+			if (currentUser.getNumber().equals("admin")) {
+				return true;
+			} else {
+				// 没有登陆，转向登陆界面
+				request.getRequestDispatcher("/login").forward(request, response);
+				return false;
+			}
+		} else 
+			return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+
+	}
+
+}
